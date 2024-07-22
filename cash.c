88aab216ae48c56376994fc9116e5c35e2e31688
José Antonio Rosales <jose.rosales.mald@gmail.com>
2024-07-21 20:20:48 -0600

    implemented tokenize and launch functions

--- cash.c
+++ cash.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "cash.h"
 #include "constants.h"
 
@@ -26,7 +29,7 @@ char *read_line(void) {
         if (feof(stdin)) {
             exit(SUCCESS);
         } else {
-            perror("Failed to read line");
+            fprintf(stderr, "cash: failed to read line");
             exit(ERROR);
         }
     }
@@ -34,7 +37,62 @@ char *read_line(void) {
 }
 
 char **tokenize(char *line) {
-    // TODO
+    int bufsz = CASH_TOK_BUFSIZE;
+    int position = 0;
+    char **tokens = malloc(bufsz * sizeof(char *));
+    char *token;
+
+    if (tokens == NULL) {
+        fprintf(stderr, "cash: allocation error");
+        exit(ERROR);
+    }
+
+    token = strtok(line, CASH_TOK_DELIM);
+
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsz) {
+            bufsz += CASH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsz * sizeof(char*));
+            if (tokens == NULL) {
+                fprintf(stderr, "cash: re-allocation error\n");
+                exit(ERROR);
+            }
+
+        }
+
+        token = strtok(NULL, CASH_TOK_DELIM);
+    }
+
+    tokens[position] = NULL;
+    return tokens;
+}
+
+int launch(char **args) {
+    pid_t pid;
+    pid_t wpid;
+    int status;
+
+    pid = fork();
+    // Child
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            fprintf(stderr, "cash: forking error\n");
+        }
+        exit(ERROR);
+    } else if (pid < 0) {
+        fprintf(stderr, "cash: forking error\n");
+        exit(ERROR);
+    // Parent
+    } else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
 }
 
 int exec(char **args) {
--- constants.h
+++ constants.h
@@ -1,6 +1,9 @@
 #ifndef CASH_C_CONSTANTS_H
 #define CASH_C_CONSTANTS_H
 
+#define CASH_TOK_BUFSIZE 8
+#define CASH_TOK_DELIM " \t\r\n\a"
+
 #define SUCCESS 0
 #define ERROR 1
 
